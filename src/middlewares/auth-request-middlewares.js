234b383d152812e8698d99796a7321de23ec5571
iitiansandy <sandeep.sid.kumar10@gmail.com>
2024-08-24 19:43:35 +0530

    added auth req middleware

---
+++ src/middlewares/auth-request-middlewares.js
@@ -0,0 +1,24 @@
+const { StatusCodes } = require('http-status-codes');
+
+const { ErrorResponse } = require('../utils/common');
+const AppError = require('../utils/errors/app-error');
+
+function validateAuthRequest(req, res, next) {
+    if (!req.body.name) {
+        ErrorResponse.message = 'Something went wrong while creating city';
+        
+        ErrorResponse.error = new AppError(['Email not found in the incoming request in the correct form'], StatusCodes.BAD_REQUEST);
+        return res.status(StatusCodes.BAD_REQUEST).send({ErrorResponse});
+    };
+
+    if (!req.body.password) {
+        ErrorResponse.message = 'Something went wrong while authenticating user';
+        
+        ErrorResponse.error = new AppError(['Password not found in the incoming request in the correct form'], StatusCodes.BAD_REQUEST);
+        return res.status(StatusCodes.BAD_REQUEST).send({ErrorResponse});
+    };
+
+    next();
+};
+
+module.exports = {validateAuthRequest};
--- src/routes/v1/user-routes.js
+++ src/routes/v1/user-routes.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-const { UserController } = require('../../controllers')
+const { UserController } = require('../../controllers');
+
+const { AuthRequestMiddleware } = require('../../middlewares');
 
 // USER SIGNUP
-router.post('/signup', UserController.signup);
+router.post('/signup', AuthRequestMiddleware.validateAuthRequest, UserController.signup);
 
 // USER SIGNIN
-router.post('/signin', UserController.signin);
+router.post('/signin', AuthRequestMiddleware.validateAuthRequest, UserController.signin);
 
 
 
